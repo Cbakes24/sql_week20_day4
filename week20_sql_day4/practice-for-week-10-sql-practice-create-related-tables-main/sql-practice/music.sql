09997a4b7e7fd71d7f717fc804dccf15d75397af
Derek Emsbach <demsbach92@gmail.com>
2022-08-11 21:43:47 -0600

    Create tables

--- week20_sql_day4/practice-for-week-10-sql-practice-create-related-tables-main/sql-practice/music.sql
+++ week20_sql_day4/practice-for-week-10-sql-practice-create-related-tables-main/sql-practice/music.sql
@@ -3,6 +3,7 @@ PRAGMA foreign_keys = 1;
 DROP TABLE IF EXISTS instruments;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
+DROP TABLE IF EXISTS musicians_instruments;
 
 CREATE TABLE bands (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,9 +12,27 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  bands_id VARCHAR(100),
+  FOREIGN KEY (bands_id) REFERENCES bands(id) on DELETE CASCADE
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+
+INSERT INTO bands (name)
+VALUES
+('Jam Packed');
+
+INSERT INTO musicians (first_name, last_name, bands_id)
+VALUES
+('Olive', 'EMSBACH', 1),
+('Cory', 'Baker', 1);
+
+CREATE TABLE musicians_instruments (
+  musician_id INTEGER,
+  instrument_id INTEGER,
+  FOREIGN KEY (musician_id) REFERENCES musicians(id)
+  FOREIGN KEY (instrument_id) REFERENCES instruments(id)
 );
